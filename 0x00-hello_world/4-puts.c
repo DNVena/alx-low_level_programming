f294661ce1e7550c97f3ed135a895936acacc820
Dorcas <dnmohlala@gmail.com>
2023-04-20 13:05:07 -0700

    new file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multiligual puzzle";
+
+	puts(str);
+
+	return (0);
+}
