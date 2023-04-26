bc0eaa313ed5b600d6151ab928770e4643d22f42
Dorcas <dnmohlala@gmail.com>
2023-04-26 00:43:01 -0700

    new file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: takes integer
+ * Return: Always 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
