25079fea2ff1ba9ea2a52a8d28df56d134257c37
Dorcas <dnmohlala@gmail.com>
2023-04-26 16:34:17 -0700

    new file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @i: takes a integer
+ * Return: Always 0
+ */
+int print_last_digit(int i)
+{
+	int num;
+
+	num = i % 10;
+	return (num);
+}
