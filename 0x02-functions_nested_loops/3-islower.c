ab1b688ee1ce79f5169b590e79c7a0d1c95b61cf
Dorcas <dnmohlala@gmail.com>
2023-04-25 20:17:41 -0700

    new file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lower case
+ * @c: parameter to be checked
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
