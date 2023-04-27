023c5f5427bee04a4aa267fec553c77b0e94ae55
Dorcas <dnmohlala@gmail.com>
2023-04-26 17:27:07 -0700

    new file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: integer value
+ * @j: integer value
+ * Return: Always 0
+ */
+int add(int i, int j)
+{
+	int total;
+
+	total = i + j;
+	return (total);
+}
