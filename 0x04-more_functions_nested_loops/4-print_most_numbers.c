6f15a63d3f83ce2da191bb2d08803f6cafbe4866
Dorcas <dnmohlala@gmail.com>
2023-05-06 03:20:05 -0700

    new file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+        {
+		if (i != 50 && i != 52)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
