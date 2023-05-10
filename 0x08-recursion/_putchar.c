2c5027464cff2de167cc9a6595110eb51978cbe7
Dorcas <dnmohlala@gmail.com>
2023-05-10 01:33:23 -0700

    New directory, readme file and putchar file

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - prints characters
+ * @c: takes characters
+ * Return: Always character
+ */
+
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
