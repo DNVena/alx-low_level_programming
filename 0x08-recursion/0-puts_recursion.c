d19aae1cb375405a15ffa41b37d4f33f28e46def
Dorcas <dnmohlala@gmail.com>
2023-05-10 03:28:52 -0700

    new file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: takes character pointer
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	write(1, s, 1);
+	s++;
+	if (*s)
+	{
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+}
