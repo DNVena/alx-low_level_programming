4bbae9d5b577c8f8110bfd70008e77c12d05d4f4
Dorcas <dnmohlala@gmail.com>
2023-05-16 10:29:15 -0700

    new file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _len - finds string length
+ * @str: takes char pointer
+ * Return: int
+ */
+int _len(char *str)
+{
+	int i;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - duplicates string
+ * @str: takes pointer
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned long int len, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = _len(str);
+	arr = malloc(sizeof(char) * len);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; *str != '\0'; j++)
+	{
+		arr[j] = *str;
+		str++;
+	}
+	return (arr);
+}
