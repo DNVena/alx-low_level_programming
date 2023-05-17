34cd7009e89dd7ba655cfff367b60529162f1a74
Dorcas <dnmohlala@gmail.com>
2023-05-17 05:17:35 -0700

    new file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,6 +14,18 @@ int **alloc_grid(int width, int height)
 
 	int **arr = (int **)malloc(height * sizeof(int *));
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	if (arr == NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(arr[i]);
+		}
+		free(arr);
+	}
 	for (k = 0; k < height; k++)
 	{
 		arr[k] = (int *)malloc(width * sizeof(int));
