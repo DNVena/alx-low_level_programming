f93f579da391b2fa2c2ec156e2cd9df98cfb031f
Dorcas <dnmohlala@gmail.com>
2023-05-19 04:00:29 -0700

    new file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _prt - prints int
+ * @n: takes int
+ * Return: void
+ */
+void _prt(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		_prt(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * more_numbers - prints numbers
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			_prt(i);
+		}
+		putchar('\n');
+		j++;
+	}
+}
