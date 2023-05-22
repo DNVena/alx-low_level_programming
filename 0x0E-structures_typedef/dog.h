2b94f889ae3f08593dcd53ca3a40bfabf76e3a4c
Dorcas <dnmohlala@gmail.com>
2023-05-22 13:56:25 -0700

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - defines new type
+ * @name: takes pointer
+ * @age: float
+ * @owner: takes pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
