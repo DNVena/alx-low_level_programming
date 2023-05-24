eeea92c29531ee957626e6eebea10988a75c4798
Dorcas <dnmohlala@gmail.com>
2023-05-24 13:51:34 -0700

    new file

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates an array
+ * @array: takes pointer
+ * @size: takes size
+ * @action: takes function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
