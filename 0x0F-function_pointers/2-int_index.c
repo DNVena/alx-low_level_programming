94433b14604e12f7dda17866b45e0625e2755127
Dorcas <dnmohlala@gmail.com>
2023-05-24 14:44:16 -0700

    new file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	if (cmp != NULL && array != NULL)
 	{
-		if ((*cmp)(array[i]) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if ((*cmp)(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
-	return (0);
+	return (-1);
 }
