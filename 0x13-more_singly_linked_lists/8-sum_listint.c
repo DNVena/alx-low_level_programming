140262331a97ceade31adada017ec958e17d172e
Dorcas <dnmohlala@gmail.com>
2023-06-06 17:34:20 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sum_listint(listint_t *head)
+ * @head: pointer
+ * Return: integer
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *tem = head;
+	int sum = 0;
+
+	while (tem)
+	{
+		sum += tem->n;
+		tem = tem->next;
+	}
+	return (sum);
+}
