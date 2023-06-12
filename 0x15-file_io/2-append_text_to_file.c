1dcfb04ceae6b27f6ad85670ec76f08cba605ad3
Dorcas <dnmohlala@gmail.com>
2023-06-12 12:49:01 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * append_text_to_file - appends text
+ * @filename: takes pointer
+ * @text_content: takes pointe
+ * Return: integer
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+		{
+			;
+		}
+	}
+	 o = open(filename, O_WRONLY, O_APPEND);
+	 w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+	}
+	close(o);
+	return (1);
+}
+
