fafbe621071c3e2317e5fa74bf00cfae628ddeb0
Dorcas <dnmohlala@gmail.com>
2023-06-29 05:28:57 -0700

    new

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - checks endianness
+ * Return: integer
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
